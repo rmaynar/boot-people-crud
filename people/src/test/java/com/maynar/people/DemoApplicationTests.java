fe0a2ac93ab3969dc0feeb58b8e86e945bdb0dd3
Raúl Maynar Selgas <rmaynar@atsistemas.com>
2020-05-31 09:58:45 +0200

    spring boot initialized

---
+++ people/src/test/java/com/maynar/people/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maynar.people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
